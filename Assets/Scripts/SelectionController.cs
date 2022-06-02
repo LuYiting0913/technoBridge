5fcb0a2aae3e32730dc25354057e4fd472cfb36b
Lu Yiting <e0774189@u.nus.edu>
2022-06-02 11:25:09 +0800

    Completed box selection and deletion;  cleaned up code

--- Assets/Scripts/SelectionController.cs
+++ Assets/Scripts/SelectionController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class SelectionController : MonoBehaviour { 
     private static List<SolidBar> selectedBars = new List<SolidBar>();
@@ -19,6 +20,8 @@ public class SelectionController : MonoBehaviour {
     }
 
     public static void ClearAll() {
+        foreach (SolidBar bar in selectedBars) if (bar != null) bar.transform.GetChild(0).gameObject.SetActive(false);
+        foreach (Point point in selectedPoints) if (point != null) point.transform.GetChild(0).gameObject.SetActive(false);
         selectedBars = new List<SolidBar>();
         selectedPoints = new List<Point>();
     }
@@ -38,9 +41,42 @@ public class SelectionController : MonoBehaviour {
 
     public static void FinalizeBoxSelection() {
         SelectionBox.transform.localScale = new Vector3(0, 0, 1);
+        int scanInterval = 5;
+        int leftBound =  (int) Math.Round(Math.Min(firstCorner.x, SecondCorner.x));
+        int rightBound = (int) Math.Round(Math.Max(firstCorner.x, SecondCorner.x));
+        int lowerBound = (int) Math.Round(Math.Min(firstCorner.y, SecondCorner.y));
+        int upperBound = (int) Math.Round(Math.Max(firstCorner.y, SecondCorner.y));
+
+        for (int i = leftBound; i < rightBound; i += scanInterval) {
+            for (int j = lowerBound; j < upperBound; j += scanInterval) {
+                RaycastHit2D hit = Physics2D.Raycast(new Vector3(i, j, -10), new Vector3(0, 0, 1));
+                if (hit.collider != null) {
+                    Debug.Log(hit.transform);
+                    bool isActive = hit.transform.GetChild(0).gameObject.activeSelf;
+                    if (!isActive) {
+                        SelectionController.AddToSelection(hit.transform);
+                    }
+                    hit.transform.GetChild(0).gameObject.SetActive(true);
+                }
+            }
+        }
+
+        firstCorner = Vector2.zero;
+        SecondCorner = Vector2.zero;        
     }
 
-    public static void AddToSelection(Transform transform) {
+    public static void ToggleIndividual(RaycastHit2D hit) {
+        bool isActive = hit.transform.GetChild(0).gameObject.activeSelf;
+        hit.transform.GetChild(0).gameObject.SetActive(!isActive);
+        if (isActive) {
+            // deselect
+            SelectionController.RemoveFromSelection(hit.transform);
+        } else {
+            SelectionController.AddToSelection(hit.transform);
+        }
+    }
+
+    private static void AddToSelection(Transform transform) {
         if (transform.GetComponent<SolidBar>() != null) {
             selectedBars.Add(transform.GetComponent<SolidBar>());
         } else {
@@ -48,7 +84,7 @@ public class SelectionController : MonoBehaviour {
         }
     } 
 
-    public static void RemoveFromSelection(Transform transform) {
+    private static void RemoveFromSelection(Transform transform) {
         if (transform.GetComponent<SolidBar>() != null) {
             selectedBars.Remove(transform.GetComponent<SolidBar>());
         } else {
