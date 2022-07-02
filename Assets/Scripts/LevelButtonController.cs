e47e77c1d0a43c0082609e4c3af2e40383d894fc
heeeyi <e0772644@u.nus.edu>
2022-07-02 17:29:01 +0800

    Make login pop-ups, modified level buttons

--- Assets/Scripts/LevelButtonController.cs
+++ Assets/Scripts/LevelButtonController.cs
@@ -6,14 +6,20 @@ public class LevelButtonController : MonoBehaviour {
     public int level;
     private bool isClicked = false;
 
+    void Start() {
+		if (Levels.GetLevelData().ContainsKey(level)) {
+			GetComponent<MeshRenderer>().material.color = new Color(0, 0, 1);
+		}
+	}
+
     private void OnMouseEnter() {
         transform.position += new Vector3(0, 10, 0);
-        GetComponent<MeshRenderer>().material.color = new Color(0, 0, 1);
+        // GetComponent<MeshRenderer>().material.color = new Color(0, 0, 1);
     }
 
     private void OnMouseExit() {
         transform.position -= new Vector3(0, 10, 0);
-        GetComponent<MeshRenderer>().material.color = new Color(1, 1, 1);
+        // GetComponent<MeshRenderer>().material.color = new Color(1, 1, 1);
     }
 
     private void OnMouseDown() {
