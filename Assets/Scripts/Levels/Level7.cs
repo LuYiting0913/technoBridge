901fd9e4b996d7b8204989d9e2c15cd4ddd500ca
heeeyi <e0772644@u.nus.edu>
2022-07-05 18:22:55 +0800

    Add boat & hydraulics for level 7

--- Assets/Scripts/Levels/Level7.cs
+++ Assets/Scripts/Levels/Level7.cs
@@ -12,22 +12,38 @@ public class Level7 : MonoBehaviour {
         PointReference p2 = new PointReference();
         PointReference p3 = new PointReference();
         PointReference p4 = new PointReference();
+        PointReference p5 = new PointReference();
+        PointReference p6 = new PointReference();
+        PointReference p7 = new PointReference();
+        PointReference p8 = new PointReference();
         List<Vehicle> vehicles = new List<Vehicle>();
 
         p1.SetFixed();
         p2.SetFixed();
         p3.SetFixed();
         p4.SetFixed();
+        p5.SetFixed();
+        p6.SetFixed();
+        p7.SetFixed();
+        p8.SetFixed();
 
         p1.SetPosition(new Vector3(-340, -60, 0));
         p2.SetPosition(new Vector3(-60, -10, 0));
         p3.SetPosition(new Vector3(50, -10, 0));
         p4.SetPosition(new Vector3(340, -50, 0));
+        p5.SetPosition(new Vector3(-340, -130, 0));
+        p6.SetPosition(new Vector3(-440, -60, 0));
+        p7.SetPosition(new Vector3(430, -50, 0));
+        p8.SetPosition(new Vector3(340, 130, 0));
 
         fixedPoints.Add(p1);
         fixedPoints.Add(p2);
         fixedPoints.Add(p3);
         fixedPoints.Add(p4);
+        fixedPoints.Add(p5);
+        fixedPoints.Add(p6);
+        fixedPoints.Add(p7);
+        fixedPoints.Add(p8);
         Levels.UpdateLevelData(7, fixedPoints, new List<SolidBarReference>());
     }
 }
