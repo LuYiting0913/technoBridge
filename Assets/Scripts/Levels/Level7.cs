b3b1a3e5a3dbc77fb7adc4c53ed957c2d1a853ba
heeeyi <e0772644@u.nus.edu>
2022-07-05 16:55:30 +0800

    Do level 7

---
+++ Assets/Scripts/Levels/Level7.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// store all information abt this level
+public class Level7 : MonoBehaviour {
+    // private static m_Instance;
+    private static List<PointReference> fixedPoints = new List<PointReference>();
+
+    public static void InitLevel() {
+        PointReference p1 = new PointReference();
+        PointReference p2 = new PointReference();
+        PointReference p3 = new PointReference();
+        PointReference p4 = new PointReference();
+        List<Vehicle> vehicles = new List<Vehicle>();
+
+        p1.SetFixed();
+        p2.SetFixed();
+        p3.SetFixed();
+        p4.SetFixed();
+
+        p1.SetPosition(new Vector3(-340, -60, 0));
+        p2.SetPosition(new Vector3(-60, -10, 0));
+        p3.SetPosition(new Vector3(50, -10, 0));
+        p4.SetPosition(new Vector3(340, -50, 0));
+
+        fixedPoints.Add(p1);
+        fixedPoints.Add(p2);
+        fixedPoints.Add(p3);
+        fixedPoints.Add(p4);
+        Levels.UpdateLevelData(7, fixedPoints, new List<SolidBarReference>());
+    }
+}
