37cc016c98336014ca5aeaa1f868bb05fe08fb28
Lu Yiting <e0774189@u.nus.edu>
2022-07-14 11:16:01 +0800

    added level 29;

--- Assets/Scripts/Levels/Level29.cs
+++ Assets/Scripts/Levels/Level29.cs
@@ -14,10 +14,10 @@ public class Level29 : MonoBehaviour {
         PointReference p4 = new PointReference();
         PointReference p5 = new PointReference();
         PointReference p6 = new PointReference();
-        PointReference p7 = new PointReference();
-        PointReference p8 = new PointReference();
-        PointReference p9 = new PointReference();
-        PointReference p10 = new PointReference();
+        // PointReference p7 = new PointReference();
+        // PointReference p8 = new PointReference();
+        // PointReference p9 = new PointReference();
+        // PointReference p10 = new PointReference();
         // PointReference p5 = new PointReference();
 
         List<Vehicle> vehicles = new List<Vehicle>();
@@ -28,21 +28,21 @@ public class Level29 : MonoBehaviour {
         p4.SetFixed(); 
         p5.SetFixed();
         p6.SetFixed();
-        p7.SetFixed();
-        p8.SetFixed();
-        p9.SetFixed();
-        p10.SetFixed();
+        // p7.SetFixed();
+        // p8.SetFixed();
+        // p9.SetFixed();
+        // p10.SetFixed();
         // 
-        p1.SetPosition(new Vector3(-280, 40, 0));
-        p2.SetPosition(new Vector3(-280, -20, 0));
-        p3.SetPosition(new Vector3(-280, 100, 0));
-        p4.SetPosition(new Vector3(-280, 160, 0));
-        p5.SetPosition(new Vector3(280, -20, 0));
-        p6.SetPosition(new Vector3(280, 40, 0));
-        p7.SetPosition(new Vector3(280, 100, 0));
-        p8.SetPosition(new Vector3(280, 160, 0));
-        p9.SetPosition(new Vector3(100, 280, 0));
-        p10.SetPosition(new Vector3(-100, 280, 0));
+        p1.SetPosition(new Vector3(-600, -80, 0));
+        p2.SetPosition(new Vector3(-600, -20, 0));
+        p3.SetPosition(new Vector3(-700, -20, 0));
+        p4.SetPosition(new Vector3(600, -80, 0));
+        p5.SetPosition(new Vector3(600, -20, 0));
+        p6.SetPosition(new Vector3(700, -20, 0));
+        // p7.SetPosition(new Vector3(280, 100, 0));
+        // p8.SetPosition(new Vector3(280, 160, 0));
+        // p9.SetPosition(new Vector3(100, 280, 0));
+        // p10.SetPosition(new Vector3(-100, 280, 0));
 
         fixedPoints.Add(p1);
         fixedPoints.Add(p2);
@@ -50,9 +50,9 @@ public class Level29 : MonoBehaviour {
         fixedPoints.Add(p4);
         fixedPoints.Add(p5);
         fixedPoints.Add(p6);
-        fixedPoints.Add(p7);
-        fixedPoints.Add(p8);
+        // fixedPoints.Add(p7);
+        // fixedPoints.Add(p8);
         
-        Levels.UpdateLevelData(25, fixedPoints, new List<SolidBarReference>());
+        Levels.UpdateLevelData(29, fixedPoints, new List<SolidBarReference>());
     }
 }
