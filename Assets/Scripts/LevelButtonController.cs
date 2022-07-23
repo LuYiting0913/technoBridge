1d07937b1853495f7bb625c2009530d83c2c2a77
heeeyi <e0772644@u.nus.edu>
2022-07-23 19:55:06 +0800

    fix merge

--- Assets/Scripts/LevelButtonController.cs
+++ Assets/Scripts/LevelButtonController.cs
@@ -10,10 +10,10 @@ public class LevelButtonController : MonoBehaviour {
 
     void Start() {
         
-		if (Levels.IsLevelEdited(level) || GlobalData.GetLocalData(level) != 0) {
+		if (Levels.IsLevelEdited(level)) {// || GlobalData.GetLocalData(level) != 0) {
 			GetComponent<MeshRenderer>().material.color = new Color(0, 0, 1);
 		}
-        /*
+
         int star = GlobalData.GetStarLevel(level);
         if (GlobalData.GetLocalData(level) != 0) {
 	        if (star == 3) {
@@ -23,7 +23,7 @@ public class LevelButtonController : MonoBehaviour {
 	        } else if (star == 1) {
 		        GetComponent<MeshRenderer>().material.color = new Color(1, 230f/255f, 30f/255f);
 	        }
-        }*/
+        }
         // levelMenuController = transform.parent.parent.GetComponent<LevelMenuController>();
 	}
 
